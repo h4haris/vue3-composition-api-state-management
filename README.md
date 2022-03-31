b154a7dbbb053ec77adcd2d75df5b7bf602eca18
Shaikh, Muhammad Haris <muhammadharis.shaikh@ncr.com>
2022-03-31 17:02:38 +0500

    Initial Project Added

---
+++ README.md
@@ -0,0 +1,27 @@
+# Vue 3 | State Management with the Composition API 
+
+Demo application to show State Management with the Composition API
+(Alternate of Vuex in Vue 3 for small projects)
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/CounterStats.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="about">
+    <h1>Counter Stats:</h1>
+    <p>Current Count: {{ count }}</p>
+    <p>Current Color: {{ color }}</p>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+import store from "../store/index";
+
+export default {
+  name: "stats",
+  setup() {
+    const count = computed(() => store.state.count);
+    const color = computed(() => store.state.color);
+
+    return {count, color};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+h1 {
+  font-size: 40px;
+}
+
+p {
+  margin-top: 16px;
+  font-size: 24px;
+}
+</style>
